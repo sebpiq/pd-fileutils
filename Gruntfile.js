84928a82e1e1266f4a5ee910cc5a01d29f7faa61
Sébastien Piquemal <sebastien.piquemal@futurice.com>
2013-02-24 16:13:41 +0000

    added continuous integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,21 @@ var grunt = require('grunt');
 
 grunt.initConfig({
 
+  pkg: '<json:package.json>',
+  meta: {
+    banner: '/*\n'
+      + ' * <%= pkg.name %> - v<%= pkg.version %>\n'
+      + ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n'
+      + ' *\n'
+      + ' * BSD Simplified License.\n'
+      + ' * For information on usage and redistribution, and for a DISCLAIMER OF ALL\n'
+      + ' * WARRANTIES, see the file, "LICENSE.txt," in this distribution.\n'
+      + ' *\n'
+      + ' * See <%= pkg.repository.url %> for documentation\n'
+      + ' *\n'
+      + ' */\n'
+  },
+
   simplemocha: {
     options: {
       globals: ['should'],
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Pure Data file utilities
 ==========================
+
+[![Build Status](https://travis-ci.org/sebpiq/pd-fileutils.png)](https://travis-ci.org/sebpiq/pd-fileutils)
+
