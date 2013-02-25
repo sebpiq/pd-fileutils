e34f31fab671b9effba71fea5096ad233cc01ea5
Sébastien Piquemal <sebastien.piquemal@futurice.com>
2013-02-25 15:01:13 +0000

    subpatches ok

--- test/parsing-test.js
+++ test/parsing-test.js
@@ -113,10 +113,7 @@ describe('parsing', function() {
           {id: 1, proto: 'print', args: ['bla'], guiData: {x: 14, y: 34}},
         ],
         connections: [
-          {
-            sinkId: 1, sourceId: 0,
-            sinkInletId: 0, sourceOutletId: 0
-          }
+          { source: [0, 0], sink: [1, 0] }
         ]
       })
     })
@@ -139,22 +136,93 @@ describe('parsing', function() {
           + '#X obj 19 370 osc~ 440;'
         , patch = parsing.parse(patchStr)
 
-      patch.nodes[0].subpatch.nodes[0].data = roundArray(patch.nodes[0].subpatch.nodes[0].data, 1)
+      var array = patch.nodes[0].subpatch
+      patch.nodes[0].subpatch = null
+      array.nodes[0].data = roundArray(array.nodes[0].data, 1)
 
       assert.deepEqual(patch, {
         nodes: [
-          {id: 0, subpatch: {
-            nodes: [{id: 0, proto: 'table', args: ['myTable', 35], data:
-              [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1,
-              1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2, 2.1, 2.2, 2.3, 2.4,
-              2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 0, 0, 0, 0, 0]
-            }],
-            connections: []
-          }},
+          {id: 0, subpatch: null},
           {id: 1, proto: 'osc~', args: [440], guiData: {x: 19, y: 370}},
         ],
         connections: []
       })
+
+      assert.deepEqual(array, {
+        nodes: [{id: 0, proto: 'table', args: ['myTable', 35], data:
+          [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1,
+          1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2, 2.1, 2.2, 2.3, 2.4,
+          2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 0, 0, 0, 0, 0]
+        }],
+        connections: []
+      })
+    })
+
+
+    it('should parse tables rightly', function() {
+      var patchStr = '#N canvas 340 223 450 300 10;\n'
+          + '#X obj 78 81 osc~;\n'
+          + '#N canvas 447 260 450 300 mySubpatch 1;\n'
+          + '#X obj 46 39 inlet~;\n'
+          + '#X obj 47 83 delwrite~ myDel;\n'
+          + '#X obj 47 126 delread~ myDel;\n'
+          + '#X obj 48 165 outlet~;\n'
+          + '#N canvas 842 260 450 300 subSubPatch 1;\n'
+          + '#X obj 67 67 outlet~;\n'
+          + '#X obj 66 32 phasor~ -440;\n'
+          + '#X connect 1 0 0 0;\n'
+          + '#X restore 183 83 pd subSubPatch;\n'
+          + '#X connect 0 0 1 0;\n'
+          + '#X connect 2 0 3 0;\n'
+          + '#X restore 79 117 pd mySubpatch;\n'
+          + '#X obj 80 175 dac~;\n'
+          + '#X connect 0 0 1 0;\n'
+          + '#X connect 1 0 2 0;\n'
+          + '#X connect 1 0 2 1;\n'
+        , patch = parsing.parse(patchStr)
+
+      var subpatch1 = patch.nodes[1].subpatch
+        , subpatch2 = subpatch1.nodes[4].subpatch
+      patch.nodes[1].subpatch = null
+      subpatch1.nodes[4].subpatch = null
+
+      assert.deepEqual(patch, {
+        nodes: [
+          {id: 0, proto: 'osc~', args: [], guiData: {x: 78, y: 81}},
+          {id: 1, subpatch: null},
+          {id: 2, proto: 'dac~', args: [], guiData: {x: 80, y: 175}}
+        ],
+        connections: [
+          {source: [0, 0], sink: [1, 0]},
+          {source: [1, 0], sink: [2, 0]},
+          {source: [1, 0], sink: [2, 1]}
+        ]
+      })
+
+      assert.deepEqual(subpatch1, {
+        nodes: [
+          {id: 0, proto: 'inlet~', args: [], guiData: {x: 46, y: 39}},
+          {id: 1, proto: 'delwrite~', args: ['myDel'], guiData: {x: 47, y: 83}},
+          {id: 2, proto: 'delread~', args: ['myDel'], guiData: {x: 47, y: 126}},
+          {id: 3, proto: 'outlet~', args: [], guiData: {x: 48, y: 165}},
+          {id: 4, subpatch: null},
+        ],
+        connections: [
+          {source: [0, 0], sink: [1, 0]},
+          {source: [2, 0], sink: [3, 0]}
+        ]
+      })
+
+      assert.deepEqual(subpatch2, {
+        nodes: [
+          {id: 0, proto: 'outlet~', args: [], guiData: {x: 67, y: 67}},
+          {id: 1, proto: 'phasor~', args: [-440], guiData: {x: 66, y: 32}}
+        ],
+        connections: [
+          {source: [1, 0], sink: [0, 0]}
+        ]
+      })
+
     })
 
     it('should fail with an unknown element', function() {
