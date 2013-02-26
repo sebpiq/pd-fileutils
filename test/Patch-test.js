a7c1cfab20956ef07db7c0ce35d97b34ca319d68
Sébastien Piquemal <sebastien.piquemal@futurice.com>
2013-02-26 08:19:05 +0200

    Patch guessing number of portlets

--- test/Patch-test.js
+++ test/Patch-test.js
@@ -6,13 +6,39 @@ describe('Patch', function() {
   describe('#getObject', function() {
 
     it('should return the right object if existing', function() {
-      var patch = new Patch(
-        {nodes: [
-          {id: 78, attr1: 90}, {id: 56, attr1: 88}, {id: 2, attr1: 5}
-        ]}
-      )
+      var patch = new Patch({
+        nodes: [ {id: 78, attr1: 90}, {id: 56, attr1: 88}, {id: 2, attr1: 5} ]
+      })
       assert.deepEqual(patch.getObject(56), {id: 56, attr1: 88})
       assert.deepEqual(patch.getObject(2), {id: 2, attr1: 5})
+    })
+
+    it('should return null if object unknown', function() {
+      var patch = new Patch({
+        nodes: [ {id: 78, attr1: 90}, {id: 56, attr1: 88}, {id: 2, attr1: 5} ],
+      })
+      assert.equal(patch.getObject(57), null)
+    })
+
+  })
+
+  describe('#guessPortlets', function() {
+    
+    it('should guess the portlets right', function() {
+      var patch = new Patch({
+        nodes: [ {id: 78, attr1: 90}, {id: 56, attr1: 88}, {id: 2, attr1: 5}, {id: 32, attr1: 9} ],
+        connections: [
+          {source: {id: 78, port: 2}, sink: {id: 56, port: 1}},
+          {source: {id: 78, port: 1}, sink: {id: 2, port: 0}}
+        ]
+      })
+      patch.guessPortlets()
+      assert.deepEqual(patch.nodes, [
+        {id: 78, attr1: 90, inlets: 0, outlets: 3},
+        {id: 56, attr1: 88, inlets: 2, outlets: 0},
+        {id: 2, attr1: 5, inlets: 1, outlets: 0},
+        {id: 32, attr1: 9, inlets: 0, outlets: 0}
+      ])
     })
 
   })
