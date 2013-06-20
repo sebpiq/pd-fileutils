d1f5f9e034341c36f4b8e5872245dad4380786b9
Sébastien Piquemal <sebpiq@gmail.com>
2013-06-20 15:05:47 +0300

    fixed index.js

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ exports.parse = require('./lib/parsing').parse
 exports.renderSvg = require('./lib/svg-rendering').render
 exports.renderPd = require('./lib/pd-rendering').render
 exports.Patch = require('./lib/Patch')
+
+if (typeof window !== 'undefined') window.pdfu = exports
